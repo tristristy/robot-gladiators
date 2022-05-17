b89c8a3091153dd82e7344cb8872b185c923afed
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-05-17 10:53:03 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,8 +67,19 @@ var fight = function(enemyName) {
 
 //Exe Function
 for (var i = 0; i < enemyNames.length; i++){
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+    
     var pickedEnemyName = enemyNames[i];
+
     enemyHealth = 50;
+
+    //debugger;
+
   fight(pickedEnemyName);  
 }
-
+else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+}
+}
