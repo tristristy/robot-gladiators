02cc896dc811341061afee4082d698667c802f7f
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-05-19 09:50:03 -0400

    random heath and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,14 +24,15 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerHealth - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
-  
+
+      var damage = randomNumber(playerAttack - 3, playerAttack)
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -47,9 +48,9 @@ var fight = function(enemyName) {
       } else {
         window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
       }
-  
+      var damage = randomNumber(enemyAttack - 3, enemyAttack)
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -79,7 +80,7 @@ var startGame = function() {
 
       var pickedEnemyName = enemyNames[i];
 
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       
       fight(pickedEnemyName);
 
@@ -163,6 +164,12 @@ var shop = function() {
       shop();
       break;
   }
+}
+
+//Random number
+var randomNumber = function(min, max){
+  var value = Math.floor(Math.random() * (max - min + 1)) + min;
+  return value;
 }
 
 startGame();
